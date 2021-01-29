828513bf5dbea0c501851a208eb51b273c97af11
Student User <student@workstation.lab.example.com>
2021-01-29 02:54:41 -0500

    Updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, CHEECHUAN World! php version is " . PHP_VERSION . "\n";
+print "CONGRATS CHEECHUAN! You have won TOTO First Prize! \n";
 ?>
